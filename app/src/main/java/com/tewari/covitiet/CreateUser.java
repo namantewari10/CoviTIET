7beed8de6c66827f718da554745e39a0396ab834
namantewari10 <namantewari10@gmail.com>
2021-05-13 02:45:07 +0530

    Reset the speed code

--- app/src/main/java/com/tewari/covitiet/CreateUser.java
+++ app/src/main/java/com/tewari/covitiet/CreateUser.java
@@ -3,7 +3,7 @@ package com.tewari.covitiet;
 public class CreateUser {
     public CreateUser()
     {}
-    public CreateUser(String name, String email, String password, String date, String isSharing, String lat, String lng) {
+    public CreateUser(String name, String email, String password, String date, String isSharing, String lat, String lng, String speed) {
         this.name = name;
         this.email = email;
         this.password = password;
@@ -11,8 +11,9 @@ public class CreateUser {
         this.isSharing = isSharing;
         this.lat = lat;
         this.lng = lng;
+        this.speed=speed;
     }
 
-    public String name, email, password, date, isSharing, lat, lng;
+    public String name, email, password, date, isSharing, lat, lng, speed;
 
 }
